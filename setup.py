7a0a2342c609210306e4e17a0caeae3e382cc260
Frederic <f.degroef@gmail.com>
2023-08-15 14:55:56 +0200

    build rust extension from setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+from setuptools_rust import Binding, RustExtension
+
+setup(
+    name="cg",
+    version="1.0",
+    packages=["cg"],
+    rust_extensions=[
+        RustExtension(
+            "cg_rustpy.cg_rustpy", binding=Binding.PyO3, path="rust/Cargo.toml"
+        )
+    ],
+    # rust extensions are not zip safe, just like C-extensions.
+    zip_safe=False,
+)
