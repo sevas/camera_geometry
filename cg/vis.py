03d5a3511be07de067f7a026c747f03263820058
Frederic De Groef <f.degroef@gmail.com>
2025-12-14 22:26:08 +0100

    project the bunny object

--- cg/vis.py
+++ cg/vis.py
@@ -1,7 +1,13 @@
 from dataclasses import dataclass
 import pyqtgraph as pg
 import pyqtgraph.opengl as gl
+from pyqtgraph.Qt import QtGui
 import numpy as np
+import plyfile
+from pathlib import Path
+
+from cg.math3d import rot_z, rot_y
+from tests.test_project_points import DATA_DIR
 
 
 @dataclass
@@ -71,6 +77,17 @@ def compute_pixel_rays(
     return origins, dirs
 
 
+
+def load_ply(fpath: str | Path) -> plyfile.PlyData:
+    with open(fpath, "rb") as f:
+        return plyfile.PlyData.read(f)
+
+DATA_DIR = Path(__file__).parent.parent / "data"
+bunny = DATA_DIR / "bun_zipper_res4.ply"
+
+
+
+
 def main():
     app = pg.mkQApp()
     # create 3d view
@@ -79,8 +96,10 @@ def main():
     view.setWindowTitle("3D View")
     view.resize(1280, 720)
 
-    g = gl.GLGridItem()
+    g = gl.GLGridItem(size=QtGui.QVector3D(2000,2000,1))
+    g.setSpacing(100, 100, 1)
     view.addItem(g)
+
 
     cam_int = CameraIntrinsics(
         fx=1000,
@@ -109,6 +128,10 @@ def main():
     #     p1=0.02,
     #     p2=0.03,
     # )
+
+    ax = gl.GLAxisItem(size=QtGui.QVector3D(1500,1500,1500))
+    view.addItem(ax)
+    ax.translate(0,0,-cam_int.fx)
 
     origins, dirs = compute_pixel_rays(
         cam_int.fx,
@@ -160,17 +183,52 @@ def main():
         return points
 
     obj_colors = [
-        (0, 1, 0, 0.7),
-        (0, 0, 1, 0.7),
+        (0, 0.6, 0, 0.7),
+        (0, 1, 1, 0.7),
         (1, 1, 0, 0.7),
         (1, 0, 1, 0.7),
+        (0.5, 0.2, 0.4, 0.7),
     ]
 
+    pixel_pitch = 3e-6
+    def d2px(meters: float) -> float:
+        return meters / pixel_pitch
+
+    bunny_ply = load_ply(bunny)
+    bunny_pcl = np.stack(
+        [
+            bunny_ply["vertex"].data["x"],
+            bunny_ply["vertex"].data["y"],
+            bunny_ply["vertex"].data["z"],
+        ],
+        axis=-1,
+    )
+
+    print("Bunny pcl shape:", bunny_pcl.shape)
+    bunny_pcl_r = bunny_pcl.copy()
+    bunny_pcl_r *= (d2px(0.3), d2px(0.3), d2px(0.3))
+
+    theta = 1 * np.pi
+    bunny_pcl_r = (rot_z(theta) @ bunny_pcl_r.T).T
+    theta_y = np.pi
+    bunny_pcl_r = (rot_y(theta_y) @ bunny_pcl_r.T).T
+    #
+    # bunny_pcl_r[:, 0] -= d2px(0.003)
+    bunny_pcl_r[:, 1] += d2px(0.007)
+    bunny_pcl_r[:, 2] += d2px(0.1)
+    # bunny_pcl_r *= d2px(0.1)
+
+    # bunny_pcl *= 10
+    # bunny_pcl_r[:, 1] += d2px(0.05)
+    # bunny_pcl_r[:, 2] += d2px(0.05)
+
+
     planes = [
-        make_plane_points(1000, 10, pos=(0, 1000, 2000)),
-        make_plane_points(2000, 20, pos=(0, -1000, 5000)),
-        make_plane_points(1000, 10, pos=(1000, 0, 2500)),
-        make_plane_points(1000, 10, pos=(-1000, 0, 1500)),
+        make_plane_points(d2px(0.1), 10, pos=(0, d2px(0.2), d2px(0.3))),
+        make_plane_points(d2px(0.2), 20, pos=(0, -d2px(0.3), d2px(0.9))),
+        make_plane_points(d2px(0.3), 10, pos=(-d2px(0.3), 0, d2px(0.5))),
+        make_plane_points(d2px(0.4), 10, pos=(d2px(0.3), 0, d2px(0.7))),
+        bunny_pcl_r
     ]
     plane_plots = []
 
@@ -181,9 +239,9 @@ def main():
         uvw[:, 2] = -cam_int.fx # points[:, 2]
         return uvw[:, :3]
 
-    for i, p in enumerate(planes[:]):
+    for i, p in enumerate(planes[4:]):
         p = p.reshape(-1, 3)
-        s = gl.GLScatterPlotItem(pos=p.reshape(-1, 3), size=50, color=obj_colors[i], pxMode=False)
+        s = gl.GLScatterPlotItem(pos=p.reshape(-1, 3), size=1000, color=obj_colors[i], pxMode=False)
         view.addItem(s)
         plane_plots.append(s)
 
